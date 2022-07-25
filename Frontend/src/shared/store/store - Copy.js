3a6730130d7191527c246674c5f3760860ab6253
Diep, Ly Bao Long <60988318+JohnnyDiep1021@users.noreply.github.com>
2022-07-25 19:58:58 +0700

    Revert "Update_250722_Reupload-whole-project"
    This reverts commit 5d34b4d06374cda5e89ec5a18c6bb165efdb489c.

--- Frontend/src/shared/store/store - Copy.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth";
-import movieReducer from "./movie";
-
-const store = configureStore({
-  reducer: { auth: authReducer, movie: movieReducer },
-});
-
-export default store;
--- Frontend/src/shared/util/util - Copy.js
+++
@@ -1,6 +0,0 @@
-export const NAME_MINLENGTH = 2;
-export const NAME_MAXLENGTH = 23;
-export const USERNAME_MINLENGTH = 6;
-export const USERNAME_MAXLENGTH = 36;
-export const EMAIL_MINLENGTH = 3;
-export const EMAIL_MAXLENGTH = 60;
