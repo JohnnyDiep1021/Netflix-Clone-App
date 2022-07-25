34b3ab6e5328679b108616df82c651891d9800ae
Diep Ly Bao Long <longdiep10102001@gmail.com>
2022-07-24 22:34:51 -0400

    update_240722_movie-detail

--- Frontend/src/HomePage/components/MovieDetail/MovieDetail.js
+++ Frontend/src/HomePage/components/MovieDetail/MovieDetail.js
@@ -3,21 +3,29 @@ import React from "react";
 import Button from "../../../shared/components/UI/Button/Button";
 
 import Modal from "../../../shared/components/UI/Modal/Modal";
+
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import AddIcon from "@mui/icons-material/Add";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 
 import "./MovieDetail.scss";
 
 const MovieDetail = (props) => {
   return (
-    <Modal className="modal-movie-detail" show={true}>
+    <Modal className="modal-movie-detail" show={false}>
       <div className="top">
-        <video controls autoPlay loop />
-        {/* <div className="img-title-container">
-          <img />
-        </div> */}
+        <div className="poster-video-container">
+          <div className="bg-filter"></div>
+          <video
+            src="https://firebasestorage.googleapis.com/v0/b/netflix-76544.appspot.com/o/images%2F1657793609946-ANH%20T%C3%9A%20-%20%20Cho%20con%20tim%20m%E1%BB%99t%20l%C3%BD%20do%20%5BOFFICIAL%20MV%5D.mp4?alt=media&token=05e2bc5e-3f22-40a3-8e07-d2d1248595b6"
+            autoPlay
+            loop
+            muted
+            className="poster-video"
+          />
+        </div>
         <div className="intrt-btn-container">
           <div className="left">
             <Button className="btn-func-icon striking">
@@ -34,7 +42,11 @@ const MovieDetail = (props) => {
               <ThumbDownOffAltIcon />
             </Button>
           </div>
-          <div className="right"></div>
+          <div className="right">
+            <Button className="btn-icon">
+              <VolumeUpIcon />
+            </Button>
+          </div>
         </div>
       </div>
       <div className="bottom">
