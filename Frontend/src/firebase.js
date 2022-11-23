80afbb0d98ff3a1c154ee39e9645d01b38a63fba
Diep Ly Bao Long <longdiep10102001@gmail.com>
2022-11-23 10:58:39 -0500

    Update_231122_reorganize-project-folder

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -12,7 +12,15 @@ const firebaseConfig = {
   appId: "1:107461363030:web:f97ae48525b10fa813c2f7",
   measurementId: "G-8WVNWTT9J7",
 };
-
+// const firebaseConfig = {
+//   apiKey: `${process.env.FIREBASE_APIKEY}`,
+//   authDomain: "netflix-clone-app-e2815.firebaseapp.com",
+//   projectId: "netflix-clone-app-e2815",
+//   storageBucket: "netflix-clone-app-e2815.appspot.com",
+//   messagingSenderId: "436186971723",
+//   appId: "1:436186971723:web:9b6838ced80052c5c117ce",
+//   measurementId: "G-TWP3XQB19N",
+// };
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 export default storage;
